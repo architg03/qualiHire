fe37fae790fd7d97077b42a01e13a888813c7ba9
BathtubLarry <67934216+BathtubLarry@users.noreply.github.com>
2022-11-09 11:06:45 -0500

    modified:   qualidii/src/qlt/src/Pages/dashboard.js

--- qualidii/src/qlt/src/Pages/dashboard.js
+++ qualidii/src/qlt/src/Pages/dashboard.js
@@ -1,64 +1,102 @@
 import React from "react";
-import {useState, useEffect} from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Accordion from 'react-bootstrap/Accordion';
+import { useState, useEffect } from "react";
+import Badge from "react-bootstrap/Badge";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Accordion from "react-bootstrap/Accordion";
 import DataCard from "../Components/datacard";
 import Container from "react-bootstrap/Container";
-import axios from "../API/axiosconfig"
+import axios from "../API/axiosconfig";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
-
 const Dashboard = (props) => {
-
   const [products, setProducts] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     const checkData = async () => {
       const res = await axios.get("/users/search", {
-        data: {firstName: `${props.user.fname}`, lName: `${props.user.lname}`}
-      })
+        data: {
+          firstName: `${props.user.fname}`,
+          lName: `${props.user.lname}`,
+        },
+      });
       const user = JSON.parse(res.request.response)[0];
       setProducts(user.products);
-    }
+    };
     checkData();
   }, []);
- 
 
   const myDataCards = products.map((product) => {
-       return (
-         <div key={product.id}>
-           <DataCard 
-           ownerID={props.user.userID}  
-           userLoggedIn={props.user.userID}
-           id={product.id}
-           name={product.name}
-           type={product.type}
-            />
-         </div>
-      );
+    return (
+      <div key={product.id}>
+        <DataCard
+          ownerID={props.user.userID}
+          userLoggedIn={props.user.userID}
+          id={product.id}
+          name={product.name}
+          type={product.type}
+        />
+      </div>
+    );
   });
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Container style={{ marginTop: "60px", paddingBottom: "50px" }}>
-      <Row>
-        <Col>
-          <Container>
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        dialogClassName="modal-70w"
+        scrollable
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>All Data</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>{myDataCards}</div>
+        </Modal.Body>
+      </Modal>
+      <Container style={{ marginTop: "60px", paddingBottom: "50px" }}>
+        <Row>
+          <Col>
+            <Container>
+              <div style={{ height: "60px" }}></div>
+              <Container>
+                <h2>My Data</h2>
+                <Accordion>
+                  <Accordion.Item eventKey="all" />
+                  <Accordion.Header>
+                    All Products
+                    <Badge style={{ Padding: "1%", marginLeft: "1%" }}>
+                      {products.length}
+                    </Badge>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div style={{ paddingBottom: "2%" }}>
+                      Contains all data from any profile that you have reserved.
+                    </div>
+                    <Button onClick={handleShow}>View All Data</Button>{" "}
+                    <Button variant="danger">Release All Data</Button>
+                  </Accordion.Body>
+                </Accordion>
+              </Container>
+            </Container>
+          </Col>
+          <Col>
             <div style={{ height: "60px" }}></div>
             <Container>
-              <h2>My Data</h2>
-              {myDataCards}
+              <h2>My Data Profile Progress</h2>
+              <p>To be added</p>
             </Container>
-          </Container>
-        </Col>
-        <Col>
-          <div style={{ height: "60px" }}></div>
-          <Container>
-            <h2>My Data Profile Progress</h2>
-            <p>To be added</p>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
