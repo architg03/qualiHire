8d076ccdb7cada5dbb6441c00417232f67f4f6c9
architg03 <119711373+architg03@users.noreply.github.com>
2023-04-26 18:20:35 -0400

    Update dashboard.js

--- qualidii/src/qlt/src/Pages/dashboard.js
+++ qualidii/src/qlt/src/Pages/dashboard.js
@@ -10,6 +10,92 @@ import axios from "../API/axiosconfig";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
+const getRole = (job) => {  // function to get the level of each job
+  job = job.toLowerCase();
+  let pos = 0;
+  const h = ["regular", "senior", "junior", "lead"];
+
+  if (job.includes("senior") || job.includes("sr")) {
+    pos = 1;
+  }
+
+  if (job.includes("lead")) {
+    if (job.includes("team lead")) {
+      return "B2";
+    } else {  // probably specialist
+      return "C1";
+    }
+  }
+
+  if (job.includes("junior") || job.includes("jr")) {
+    pos = 2;
+    console.log("jr");
+    return "A1";
+  }
+
+  if (job.includes("corporate") && job.includes("executive")) {
+    return "F1";
+  }
+
+  if (job.includes("vice") && job.includes("executive") && job.includes("president")) {
+    return "E2";
+  }
+
+  if (job.includes("vice") && job.includes("president") && pos == 1) {
+    return "E1";
+  }
+
+  if (job.includes("vice") && job.includes("associate") && job.includes("president")) {
+    return "D1";
+  }
+
+  if (job.includes("vice") && job.includes("president")) {
+    return "D2";
+  }
+
+  if (job.includes("director")) {
+    return "C3";
+  }
+
+  if ((pos == 1 && (job.includes("manager") || job.includes("architect"))) ||
+      ((job.includes("principal") || job.includes("princ")) && job.includes("specialist"))) {
+    console.log(h[pos]);
+    return "C2";
+  }
+
+  if ((job.includes("manager") || job.includes("architect")) && pos == 0) {
+    console.log(h[pos]);
+    return "C1";
+  }
+
+  if (pos == 1 && job.includes("specialist")) {
+    console.log(h[pos]);
+    return "B2";
+  }
+
+  if (pos == 1 || job.includes("specialist")) {
+    console.log(h[pos]);
+    return "B1";
+  }
+
+  if (job.includes("associate") || job.includes("engineer") || job.includes("assoc") && pos == 0) {
+    console.log(h[pos]);
+    return "A2";
+  }
+
+  if (job.includes("graduate") || job.includes("trainee")) {
+    console.log(h[pos]);
+    return "A1";
+  } else {
+    console.log(h[pos]);
+    return "A2";
+  }
+}
+
+
+
+
+
 const Dashboard = (props) => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -68,34 +154,78 @@ const Dashboard = (props) => {
           <Col>
             <Container>
               <div style={{ height: "60px" }}></div>
-              <Container>
-                <h2>My Data</h2>
-                <Accordion>
-                  <Accordion.Item eventKey="all" />
-                  <Accordion.Header>
-                    All Products
-                    <Badge style={{ Padding: "1%", marginLeft: "1%" }}>
-                      {products.length}
-                    </Badge>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    <div style={{ paddingBottom: "2%" }}>
-                      Contains all data from any profile that you have reserved.
-                    </div>
-                    <Button onClick={handleShow}>View All Data</Button>{" "}
-                    <Button variant="danger">Release All Data</Button>
-                  </Accordion.Body>
-                </Accordion>
-              </Container>
+            </Container>
+            <Container>
+              <h2 style={{ marginBottom: "0" }}>Job Requisitions</h2>
+              <div style={{ paddingBottom: "2%" }}>
+                Select which countries to get Job Requisitions from:
+              </div>
+              <select defaultValue="United States">
+                <option value="Argentina">Argentina</option>
+                <option value="Canada">Canada</option>
+                <option value="Germany">Germany</option>
+                <option value="India">India</option>
+                <option value="Israel">Israel</option>
+                <option value="Romania">Romania</option>
+                <option value="United Kingdom">United Kingdom</option>
+                <option value="United States">United States</option>
+                <option value="Portugal">Portugal</option>
+              </select>
+              <div style={{ paddingTop: "2%" }}>
+                <div style={{ paddingBottom: "2%" }}>
+                  Filter by job types:
+                </div>
+                <select defaultValue="All">
+                  <option value="All">All</option>
+                  <option value="Full-time">Full-time</option>
+                  <option value="Part-time">Part-time</option>
+                  <option value="Internship">Internship</option>
+                  <option value="Freelance">Freelance</option>
+                </select>
+                <div style={{ paddingBottom: "2%" }}>
+                  What job level:
+                </div>
+                <select defaultValue="All">
+                  <option value="All">All</option>
+                  <option value="Entry Level">Entry Level</option>
+                  <option value="Mid Level">Mid Level</option>
+                  <option value="Senior Level">Senior Level</option>
+                </select>
+                <div style={{ paddingBottom: "2%" }}>
+                  Specify Job:
+                </div>
+                <select defaultValue="">
+                  <option value="">Select a job</option>
+                  <option value="Job 1">Job 1</option>
+                  <option value="Job 2">Job 2</option>
+                  <option value="Job 3">Job 3</option>
+                  <option value="Job 4">Job 4</option>
+                </select>
+              </div>
             </Container>
           </Col>
           <Col>
-            <div style={{ height: "60px" }}></div>
-            <Container>
-              <h2>My Data Profile Progress</h2>
-              <p>To be added</p>
-            </Container>
-          </Col>
+                      <Container>
+                        <div style={{ height: "60px" }}></div>
+                      </Container>
+                      <Container>
+                        <h2>Resume Data</h2>
+                        <div style={{ paddingBottom: "2%" }}>
+                          Select which countries to get Resume Data from:
+                        </div>
+                        <select defaultValue="United States">
+                          <option value="Argentina">Argentina</option>
+                          <option value="Canada">Canada</option>
+                          <option value="Germany">Germany</option>
+                          <option value="India">India</option>
+                          <option value="Israel">Israel</option>
+                          <option value="Romania">Romania</option>
+                          <option value="United Kingdom">United Kingdom</option>
+                          <option value="United States">United States</option>
+                          <option value="Portugal">Portugal</option>
+                        </select>
+                      </Container>
+                    </Col>
         </Row>
       </Container>
     </>
